47879909a09a37e7c7b1b5e6c6eb294d2f764531
Diana <maritimdiane96@gmail.com>
2022-11-21 23:34:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 =31;
+const num2 =2;
+const multiply =num1 * num2;
+
+const random = 3
+
+const num3 = 10;
+const num4 = 6;
+const mod =num3 - num4;
+
+const max = 20;
+Math.max(1, 4, 15 ,18, 20);
